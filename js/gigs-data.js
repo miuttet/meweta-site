3d11da0371da8531b3b51461c9b14d8e3023f2a4
miuta <kaerum9628@gmail.com>
2026-02-17 00:24:06 +0900

    gigs-data.js で gigs/index を共通データから自動生成

---
+++ js/gigs-data.js
@@ -0,0 +1,100 @@
+var GIGS = [
+  {
+    date: "2026-02-20",
+    event: "delfy",
+    venue: "中野HeavySick Zero",
+    time: "OPEN 23:00",
+    artists: [
+      { role: "DJ", names: "cfnslptu / ato夢 / meweta / mionn / ShoN / 神品密売サービス" }
+    ]
+  },
+  {
+    date: "2026-02-12",
+    event: "CARESS ROOM",
+    venue: "FORESTLIMIT",
+    time: "OPEN 18:30",
+    artists: [
+      { role: "live", names: "Jesse Ruins / momonesuko / Pukha Pukha" },
+      { role: "DJ", names: "ansui / icopalmer / 津田 / meweta" }
+    ]
+  },
+  {
+    date: "2026-01-28",
+    event: "ADDIFY vol.4",
+    venue: "中野HeavySick Zero",
+    time: "OPEN 19:00 / CLOSE 23:00",
+    artists: [
+      { role: "DJ", names: "meweta / CUE KITAZAWA / M0bca5 B2B Realgal Alcoólico / Madeinl B2B ShoN" },
+      { role: "VJ", names: "嘘の名前" }
+    ]
+  },
+  {
+    date: "2026-01-24",
+    event: "ONESIXTY",
+    venue: "Neo Shinjuku Atsushi",
+    time: "OPEN 24:00 / CLOSE 06:00",
+    artists: [
+      { role: "DJ", names: "OYUBI / FETUS / MEWETA / VOX / SAZABLIZRY / SZO" }
+    ]
+  },
+  {
+    date: "2026-01-24",
+    event: "DEJI!!",
+    venue: "新宿SPACE",
+    time: "OPEN 23:00 / CLOSE 06:00",
+    artists: [
+      { role: "DJ", names: "meweta / S3R4PH / MARS89 / 宇宙チンチラ / Yurushite Nyan / Minami-no-shima" },
+      { role: "VJ", names: "Cue Kitazawa" }
+    ]
+  },
+  {
+    date: "2026-01-24",
+    event: "agul 1st Album \"Forecast\" Release Party",
+    venue: "恵比寿BATICA",
+    time: "OPEN 16:30 / CLOSE 22:00",
+    artists: [
+      { role: "release live", names: "agul" },
+      { role: "live", names: "Ash Won / iga / Xamd / in-mark / CALLMEJEI / Child Plate Peops" },
+      { role: "DJ", names: "Gen Yamada / カゲヤマ / ユウゾウ / KENMA / Lil Peace / 超越×0xygen / meweta / tragicriver" }
+    ]
+  },
+  {
+    date: "2026-01-22",
+    event: "SNK × current",
+    venue: "恵比寿BATICA",
+    time: "OPEN 18:00",
+    artists: [
+      { role: "live", names: "8bit Gang untitled01 / Chapa the DRO initiation / FOREVER THE WORLD" },
+      { role: "DJ", names: "0xygen / meweta / S3R4PH / NOWBU / jingan / ullpo" }
+    ]
+  },
+  {
+    date: "2026-01-17",
+    event: "違法人",
+    venue: "幡ヶ谷ニューマタンゴ",
+    time: "OPEN 21:00",
+    artists: [
+      { role: "DJ", names: "pawpaw / kanachanya / omokage / joseshimo / meweta / attkh~~~i" }
+    ]
+  },
+  {
+    date: "2026-01-16",
+    event: "NEOPULES",
+    venue: "FORESTLIMIT",
+    time: "OPEN 23:00 / CLOSE 05:30",
+    artists: [
+      { role: "DJ", names: "RANNA / nacogotoh / meweta / xamd / pointhope / MUNÉO × piccolo / unkokitty" }
+    ]
+  },
+  {
+    date: "2026-01-16",
+    event: "Oracle 1st anniversary",
+    venue: "theater guild 代官山",
+    time: "OPEN 17:00 / CLOSE 05:00",
+    artists: [
+      { role: "live", names: "Djubumba×超越 / KAIRUI / Telematic Visions" },
+      { role: "DJ", names: "芥川 / 離 / anolis / dj woahhaus / Lee / nano odorine / Navi FlashBackAtmosphere / meweta / Ryota Nakamura / noryo / cfnslptu" },
+      { role: "VJ", names: "avys / O.G.I / 葉山" }
+    ]
+  }
+];
